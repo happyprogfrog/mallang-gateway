1dceed89e0a8b9f7a3085773826551355f92858c
happyprogfrog <happyprogfrog@gmail.com>
2024-07-02 17:31:39 +0900

    feat: API 게이트웨이와 주울 구현

--- src/main/java/me/progfrog/mallang_gateway/MallangGatewayApplication.java
+++ src/main/java/me/progfrog/mallang_gateway/MallangGatewayApplication.java
@@ -2,8 +2,10 @@ package me.progfrog.mallang_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class MallangGatewayApplication {
 
 	public static void main(String[] args) {
