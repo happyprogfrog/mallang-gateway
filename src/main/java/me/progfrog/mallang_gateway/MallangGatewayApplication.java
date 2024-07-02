e955f9ddf299ecf1fd98f7642c9368376ca7977e
happyprogfrog <happyprogfrog@gmail.com>
2024-07-02 22:30:34 +0900

    feat: 서비스 디스커버리 구현 및 로드 밸런싱 전략을 미세 조정하기
    - 게이트웨이도 디스커버리 클라이언트로, 레지스트리와 서비스 이름을 특정 주소로 매핑할 수 있으므로 주울과 유레카 서버를 연결할 수 있다.
    - 기본 상태 체크 메커니즘을 변경하는 IPing과 기본 로드 밸런싱 전략을 수정하는 IRule 두 개의 스프링 빈을 설정하여 서비스에 핑을 보내고 결과에 따라 로드 밸런싱을 한다.

--- src/main/java/me/progfrog/mallang_gateway/MallangGatewayApplication.java
+++ src/main/java/me/progfrog/mallang_gateway/MallangGatewayApplication.java
@@ -1,11 +1,16 @@
 package me.progfrog.mallang_gateway;
 
+import me.progfrog.mallang_gateway.configuration.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
+@SpringBootApplication
 public class MallangGatewayApplication {
 
 	public static void main(String[] args) {
