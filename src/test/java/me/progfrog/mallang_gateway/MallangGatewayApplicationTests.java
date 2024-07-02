5a35649fe43651e9cb2a66dcbfc5f7388c8c32af
happyprogfrog <happyprogfrog@gmail.com>
2024-07-02 13:34:05 +0900

    프로젝트 생성

---
+++ src/test/java/me/progfrog/mallang_gateway/MallangGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.progfrog.mallang_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallangGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
